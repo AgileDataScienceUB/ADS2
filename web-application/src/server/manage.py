9fa16cfe8e996211b33d041f32f6a868be7e6422
alexfcub <31991088+alexfcub@users.noreply.github.com>
2017-11-26 13:17:55 +0100

    stupid commit

--- web-application/src/server/manage.py
+++ web-application/src/server/manage.py
@@ -7,8 +7,7 @@ manager = Manager(app)
 @manager.command
 def runserver():
     """Run the flask app in development mode"""
-#    app.run('0.0.0.0', 8081)
-    app.run()
+    app.run('0.0.0.0', 8081)
      
 if __name__ == "__main__":
     manager.run()

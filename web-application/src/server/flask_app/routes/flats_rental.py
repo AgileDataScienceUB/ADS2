9bdbc67f93ae5037ca194de31493d4b86c3fea0b
alexfcub <31991088+alexfcub@users.noreply.github.com>
2017-11-26 16:14:15 +0100

    correct csv parser for flats_rental
    clean the code and point to idealista csv, the only one available  at server/data

--- web-application/src/server/flask_app/routes/flats_rental.py
+++ web-application/src/server/flask_app/routes/flats_rental.py
@@ -8,55 +8,18 @@ flats_rental = Blueprint('flats_rental', __name__, url_prefix='/api')
 
 @flats_rental.route('/flats_rental/', methods=['GET'])
 def get_flats_rental_data():
-
-
     resources_folder = "./data/"
-
-
-    with open(resources_folder+'plloguer2017.csv', 'r') as f:
-
-
+    with open(resources_folder+'plloguer_Idealista.csv', 'r') as f:
         reader = csv.reader(f, delimiter=',')
-        """
-        header = reader.next()
-
         csv_lines = list(reader)
-
-        print("Header -> ", header)
-        print("Data from file: ", csv_lines)
-
-        """
-
-        csv_lines = list(reader)
-
-        #print("CSV LINES: ", csv_lines)
-
+        header = csv_lines[0]
         json_object = []
-
-        tmp = {}
-        c = 0
-        for row in csv_lines:
-            if(c == 0):
-                header = row
-                print("Header: ", header)
-                c=1
-            else:
+        for row in csv_lines[1:]:
                 tmp = {}
                 for item_idx, item in enumerate(row):
-
                     if(item_idx < len(header)):
                         tmp[header[item_idx]] = item
-                try:
-                    tmp['value'] =  float(tmp['price'])
-                    json_object.append(tmp)
-                except:
-                    print("Failed for: ", row)
-                    pass
-                #data = json.dumps( [ row for row in csv_lines ] )
-
-                #print("DATA: {}".join(data))
-                #print out
-
+                json_object.append(tmp)
         json_response = json.dumps(json_object)
         return Response(json_response,
                         status=html_codes.HTTP_OK_BASIC,

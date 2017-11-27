27d16f66d18950ba8c03f4c7316e785e3811ece4
alexfcub <31991088+alexfcub@users.noreply.github.com>
2017-11-27 04:28:42 +0100

    can load any csv or json except for tweets
    we make a csv to json method and use it for flats_rental and incomes.
    we load the polygon json files with json library
    for the tweets we abandon any attempt to read from the saved json file, we'll point to mlab again

--- web-application/src/server/flask_app/routes/flats_rental.py
+++ web-application/src/server/flask_app/routes/flats_rental.py
@@ -2,7 +2,7 @@ from flask import Blueprint, Response, request
 from flask_security import auth_token_required
 from ..app_utils import html_codes, token_login
 import json
-import csv as csv
+from ..extensions import csv_to_json
 
 flats_rental = Blueprint('flats_rental', __name__, url_prefix='/api')
 
@@ -10,17 +10,7 @@ flats_rental = Blueprint('flats_rental', __name__, url_prefix='/api')
 def get_flats_rental_data():
     resources_folder = "./data/"
     with open(resources_folder+'palquiler.csv', 'r') as f:
-        reader = csv.reader(f, delimiter=',')
-        csv_lines = list(reader)
-        header = csv_lines[0]
-        json_object = []
-        for row in csv_lines[1:]:
-                tmp = {}
-                for item_idx, item in enumerate(row):
-                    if(item_idx < len(header)):
-                        tmp[header[item_idx]] = item
-                json_object.append(tmp)
-        json_response = json.dumps(json_object)
+        json_response = csv_to_json(f)
         return Response(json_response,
                         status=html_codes.HTTP_OK_BASIC,
                         mimetype='application/json')
--- web-application/src/server/flask_app/routes/incomes.py
+++ web-application/src/server/flask_app/routes/incomes.py
@@ -2,24 +2,15 @@ from flask import Blueprint, Response, request
 from flask_security import auth_token_required
 from ..app_utils import html_codes, token_login
 import json
+from ..extensions import csv_to_json
 
 incomes = Blueprint('incomes', __name__, url_prefix='/api')
 
-@incomes.route('/incomes/districts', methods=['GET'])
-def get_district_polygons_data():
-    """Get dummy data returned from the server."""
-    data = {'Incomes': ['District1', 'District2', 'District3']}
-
-    json_response = json.dumps(data)
-    return Response(json_response,
-                    status=html_codes.HTTP_OK_BASIC,
-                    mimetype='application/json')
-
 @incomes.route('/incomes/neighborhoods', methods=['GET'])
 def get_neeighborhood_polygons_data():
-    data = {'Incomes': ['Neighborhood1', 'Neighborhood2', 'Neighborhood3']}
-
-    json_response = json.dumps(data)
-    return Response(json_response,
-                    status=html_codes.HTTP_OK_BASIC,
-                    mimetype='application/json')
+    resources_folder = "./data/"
+    with open(resources_folder + 'avincome2015.csv', 'r') as f:
+        json_response = csv_to_json(f)
+        return Response(json_response,
+                        status=html_codes.HTTP_OK_BASIC,
+                        mimetype='application/json')

95b26eac609091b791d6cbb94f6dcaa7657433af
ferranespanyol <31982870+ferranespanyol@users.noreply.github.com>
2017-11-29 22:51:59 +0100

    canvis transport

--- web-application/src/server/flask_app/models/transport_graph.py
+++ web-application/src/server/flask_app/models/transport_graph.py
@@ -6,7 +6,24 @@ class TransportGraph(Base):
     def __init__(self):
         pass
 
-    def calculateRouteBetween(self, A, B):
+    def constructGraph(self):
+	    Ledgelist=pd.read_csv('./data/' +'Ledgelist.csv', header=0)#edge list
+		Lnodelist=pd.read_csv('./data/' +'Lnodelist.csv', header=0)#node list
+        #This should build the networkx instances
+		g=nx.Graph()
+		for i, node in Lnodelist.iterrows():
+			g.add_node(node['name'], node[1:].to_dict())
+		for i, elrow in Ledgelist.iterrows():
+			g.add_edge(elrow[0], elrow[1], attr_dict=elrow[2:].to_dict())
+			
+	 
+    def shortpath(self, source,target):
+		ruta=nx.shortest_path(g, source, target, weight="weight")
+		l=nx.shortest_path_length(g, source, target, weight="weight")
+		v=26.0#km/h
+		t = (l/v)*60.0+1.2*len(ruta)#Sumo 1.2 minutos de espera por cada parada 
+		
+	def calculateRouteBetween(self, A, B):
         init = gp.Point(i)
 		#finiq = gp.Point(f)
 		distsini = [gp.distance.distance(init,gp.Point(np.hstack([Lnodelist.loc[i,'Lat'],Lnodelist.loc[i,'Lon']]))).km for i in range(len(Lnodelist['Lat']))]
@@ -20,23 +37,6 @@ class TransportGraph(Base):
 		target=Lnodelist.loc[idxfin,'name']
 		t,ruta=shortpath(source,target)
 		v=4#km/h
-		tfin=((idxini/v)+(idxfin/v))+t
-		return(tfin,ruta)	
-	 
-    def shortpath(self, source,target):
-		ruta=nx.shortest_path(g, source, target, weight="weight")
-		l=nx.shortest_path_length(g, source, target, weight="weight")
-		v=26.0#km/h
-		t = (l/v)*60.0+1.2*len(ruta)#Sumo 1.2 minutos de espera por cada parada 
-		return(t,ruta)
-		
-    def constructGraph(self):
-	    Ledgelist=pd.read_csv('./data/' +'Ledgelist.csv', header=0)#edge list
-		Lnodelist=pd.read_csv('./data/' +'Lnodelist.csv', header=0)#node list
-        #This should build the networkx instances
-		g=nx.Graph()
-		for i, node in Lnodelist.iterrows():
-			g.add_node(node['name'], node[1:].to_dict())
-		for i, elrow in Ledgelist.iterrows():
-			g.add_edge(elrow[0], elrow[1], attr_dict=elrow[2:].to_dict())
-		return(g)   
+		tfin=((idxini/v)+(idxfin/v))+t	
+    
+		   

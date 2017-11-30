91a3f7511587b5c3b17f061f13b1021f6de2e3fc
czs92 <kaz1992@gmail.com>
2017-11-30 15:35:49 +0100

    Tab problems solve, the transport in root must be revised

--- web-application/src/server/flask_app/models/root.py
+++ web-application/src/server/flask_app/models/root.py
@@ -39,9 +39,9 @@ class Root:
         #falta fer
         self.fill_district_safety_statistics()
         self.fill_district_geometry()
-		
-		#Grapho transport
-		self.fill_grapho_metro()
+
+        #Grapho transport
+        #self.fill_grapho_metro()
 
 
     def initialise_districts(self):
@@ -219,15 +219,14 @@ class Root:
                 point = Point(coord[0],coord[1])
                 #print(point.x,point.y)
                 district.geometry.polygon.append(point)
-				
-	def fill_grapho_metro(self):
-		Ledgelist=pd.read_csv('./data/' +'Ledgelist.csv', header=0)#edge list
-		Lnodelist=pd.read_csv('./data/' +'Lnodelist.csv', header=0)#node list
-	    #This should build the networkx instances
-		g=nx.Graph()
-		for i, node in Lnodelist.iterrows():
-			g.add_node(node['name'], node[1:].to_dict())
-		for i, elrow in Ledgelist.iterrows():
-			g.add_edge(elrow[0], elrow[1], attr_dict=elrow[2:].to_dict())
 
-		
+
+    """def fill_grapho_metro(self):
+                            Ledgelist=pd.read_csv('./data/' +'Ledgelist.csv', header=0)#edge list
+                            Lnodelist=pd.read_csv('./data/' +'Lnodelist.csv', header=0)#node list
+                            #This should build the networkx instances
+                            g=nx.Graph()
+                            for i, node in Lnodelist.iterrows():
+                                g.add_node(node['name'], node[1:].to_dict())
+                            for i, elrow in Ledgelist.iterrows():
+                                g.add_edge(elrow[0], elrow[1], attr_dict=elrow[2:].to_dict())"""		

897e11ebbd23998db4d9af6fe3481df44d37c458
czs92 <kaz1992@gmail.com>
2017-12-13 19:48:11 +0100

    Some changes

--- web-application/src/server/flask_app/routes/recommendation.py
+++ web-application/src/server/flask_app/routes/recommendation.py
@@ -4,6 +4,7 @@ from ..app_utils import html_codes, token_login
 from ..models import Root, TransportGraph
 import json
 import numpy as np
+import pandas as pd
 from random import randint
 
 
@@ -83,29 +84,101 @@ def calculate_recommendation_test():
 					status=html_codes.HTTP_OK_BASIC,
 					mimetype='application/json')
 
+@recommendation.route('/recommendation/generate', methods=['GET'])
+def calculate_recommendation_generate():
+	"""Get dummy data returned from the server."""
+
+	lat = 41.38570
+	lng = 2.16383
+	metro = 1	#int(0 no, 1 si)
+	bus = 0		#int (0 no, 1 si)
+	max_transport_time = 100 #int
+	min_rental_price = 100 #int
+	max_rental_price = 2000 #int
+	night_live = 2 #int 0->low, 1->middium, 2->High"""
+
+	rental_prince_options = np.array([400, 500, 600, 700, 800, 900, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 5000])
+	max_transport_time_options = np.array([20,30,40,50,60,90,120]) #5,10,
+	night_live_options = np.array([0,1,2])
+	lat_lng_option = np.array([['Universitat', 41.38570, 2.16383], ['MediaPro', 41.40436, 2.19382], ['Sant Joan De Deu', 41.38364, 2.10125], ['Vall dHebron', 41.42792, 2.14186], ['LaCaixa', 41.38768, 2.12667] ])
+
+	finaldata = []
+	for max_transport_time in max_transport_time_options:
+		for min_rental_price in rental_prince_options:
+			for max_rental_price in rental_prince_options[rental_prince_options > min_rental_price]:
+				for night_live in night_live_options:
+					for name, lat, lng in lat_lng_option:
+						lat = float(lat)
+						lng = float(lng)
+						# Access model instances array.
+						data = filter_neighbourhood(max_transport_time, min_rental_price, max_rental_price, night_live,lat,lng)
+						
+						data['input'] = [name, lat, lng, max_transport_time, min_rental_price, max_rental_price, night_live]
+						print (data['input'] )
+						print (data['recommendation'])
+						finaldata.append(data)
+
+	
+	finaldataToSend = {'data' : finaldata}
+	json_response = json.dumps(finaldataToSend)
+	return Response(json_response,
+					status=html_codes.HTTP_OK_BASIC,
+					mimetype='application/json')
+
+@recommendation.route('/recommendation/generate2', methods=['GET'])
+def calculate_recommendation_generate2():
+	"""Get dummy data returned from the server."""
+
+	res = pd.DataFrame(columns= ['name', 'lat', 'lng', 'max_transport_time', 'min_rental_price', 'max_rental_price', 'night_live', 'recommendation'])
+	rental_prince_options = np.array([400, 500, 600, 700, 800, 900, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 5000])
+	max_transport_time_options = np.array([20,30,40,50,60,90,120]) #5,10,
+	night_live_options = np.array([0,1,2])
+	lat_lng_option = np.array([['Universitat', 41.38570, 2.16383], ['MediaPro', 41.40436, 2.19382], ['Sant Joan De Deu', 41.38364, 2.10125], ['Vall dHebron', 41.42792, 2.14186], ['LaCaixa', 41.38768, 2.12667] ])
+
+	finaldata = []
+	for max_transport_time in max_transport_time_options:
+		for min_rental_price in rental_prince_options:
+			for max_rental_price in rental_prince_options[rental_prince_options > min_rental_price]:
+				for night_live in night_live_options:
+					for name, lat, lng in lat_lng_option:
+						lat = float(lat)
+						lng = float(lng)
+						print([name, lat, lng, max_transport_time, min_rental_price, max_rental_price, night_live])
+						# Access model instances array.
+						data = filter_neighbourhood(max_transport_time, min_rental_price, max_rental_price, night_live,lat,lng)
+						
+						res.loc[res.shape[0]] = np.array([name, lat, lng, max_transport_time, min_rental_price, max_rental_price, night_live, data])
+
+	res.to_csv('restult.csv')
+	print(res)
+	json_response = json.dumps({'ok': 1})
+	return Response(json_response,
+					status=html_codes.HTTP_OK_BASIC,
+					mimetype='application/json')
+
+
+
+
+
 
 def filter_neighbourhood(max_transport_time, min_rental_price, max_rental_price, night_live, lat, lng):
 
 	array_possible_neighbourhoods = []
 
-	#r.neighborhoods = {'id' : Neighbourhood()}
-	#Neighbouhood = self.avg_flat_rental_from_council
-	#Neighbouhood = self.avg_flat_rental_from_web
-	#Neighbouhood = self.store_bar
-	#Neighbouhood = self.store_disco
+	ordre =np.array([-1,-1,night_live-1,1,1,1,-1])
+	addend = np.array([6,6,night_live-1,0,0,0,6])
+	dics={}
+	score={}
+	maxim=np.zeros(7)
+	minim=1e6*np.ones(7)
+
+	
 
 	#Bar values --> 25% - 17.000000 50% - 50.000000 75% - 94.000000
 	#Disco values --> 25% - 0.000000 50% - 1.000000 75% - 3.000000
 
 	for key, value in r.neighborhoods.items():
 
-		#print([value.geometry.centroid.x, value.geometry.centroid.y])
-		#print([lat, lng])
-		#print(transport_graph.calculateRouteBetween([value.geometry.centroid.y, value.geometry.centroid.x],[lat, lng]))
-		#print()
-		#print(key,value.geometry.centroid.x, value.geometry.centroid.y)
-		#print(transport_graph.calculateRouteBetween([value.geometry.centroid.x, value.geometry.centroid.y],[lat, lng]))
-		#print()
 		include = False
 		if ((value.avg_flat_rental_from_council <= max_rental_price) and (value.avg_flat_rental_from_council >= min_rental_price)) or ((value.avg_flat_rental_from_web <= max_rental_price) and (value.avg_flat_rental_from_web >= min_rental_price)):
 
@@ -126,66 +199,65 @@ def filter_neighbourhood(max_transport_time, min_rental_price, max_rental_price,
 
 
 		if include:
-			if (transport_graph.calculateRouteBetween([value.geometry.centroid.y, value.geometry.centroid.x],[lat, lng])[0] <= max_transport_time):
-				weight = randint(0, 9) #weight = compute_weight(value)
-				array_possible_neighbourhoods.append({'id':'%02d' % key, 'value': weight})
+			tTransport = transport_graph.calculateRouteBetween([value.geometry.centroid.y, value.geometry.centroid.x],[lat, lng])[0]
+			if (tTransport <= max_transport_time):
+				rental_web = value.avg_flat_rental_from_web
+				mean_size_price = value.avg_flat_meter_rental
+				night = value.store_bar + 3*value.store_disco
+				restaurants = value.store_restaurant
+				clothes_stores = value.store_clothes
+				aliment_stores = value.store_grocery
+				temps_trans= tTransport 
+
+				llista=np.array([rental_web,mean_size_price,night,restaurants,clothes_stores,aliment_stores,temps_trans])
+				score[key]=llista
+
+				maxim[llista>maxim]=llista[llista>maxim]
+				minim[llista<minim]=llista[llista<minim]
+
+	return(len(score))
+	interval = (maxim-minim)/5. +minim
+
+	#print(interval)
+	#print(score)
+	final_rank={}
+	for key in score:
+		llista = score[key]
+		rank = []
+		print(key, llista)
+		for item , fact in zip(llista,interval):
+			#----------------------------------
+			#Que fa exactament???
+			i=1
+			while item>i*fact:
+				i=i+1
+			#----------------------------------
+			rank.append(i)
+
+		#hola=rank*ordre
+		#hola[hola<0]+=6
+		final_rank[key]=list(rank*ordre)
+
+	#print(final_rank)
+
+	rank_ordre={}
+	for barri in final_rank:
+		rank_ordre[barri]=int(np.sum(final_rank[barri]))
+
+	#print(rank_ordre)
+
+	sort=(sorted(rank_ordre.items(), key=lambda x: x[1]))[::-1]
+
+	i=1
+	for item in sort:
+		array_possible_neighbourhoods.append({'id':'%02d' % item[0], 'value': i} )
+		i=i+1 if i<5 else 5
+
+	
+	return {'recommendation': array_possible_neighbourhoods}
 
 	#{'recommendation': [1,3,5,6]}
 	#{'recommendation': [{id:01, value:1}, {id:02, value:2}]}}
 
 	#'recommendation': array_possible_neighbourhoods és un diccionari amb id dels barris que compleixen i el temps de transport_cost
 	###################################
-	ordre=np.array([-1,-1,night_live-1,1,1,1,-1])
-	ordre
-	dics={}
-	for element in array_possible_neighbourhoods:
-		dics[int(element['id'])]=element['value']
-
-	score={}
-	maxim=np.zeros(7)
-	minim=1e6*np.ones(7)
-	for key, value in r.neighborhoods.items():
-
-		if key in dics:
-			rental_web = value.avg_flat_rental_from_web
-			mean_size_price = value.avg_flat_meter_rental
-			night = value.store_bar + 3*value.store_disco
-			restaurants = value.store_restaurant
-			clothes_stores = value.store_clothes
-			aliment_stores = value.store_grocery
-			temps_trans=dics[key]
-
-			llista=np.array([rental_web,mean_size_price,night,restaurants,clothes_stores,aliment_stores,temps_trans])
-			score[key]=llista
-
-			maxim[llista>maxim]=llista[llista>maxim]
-			minim[llista<minim]=llista[llista<minim]
-
-		interval = (maxim-minim)/5. +minim
-
-		final_rank={}
-		for key in score:
-			llista = score[key]
-			rank = []
-			for item , fact in zip(llista,interval):
-				i=1
-				while item>i*fact:
-					i=i+1
-				rank.append(i)
-			final_rank[key]=list(rank*ordre)
-
-	rank_ordre={}
-	for barri in final_rank:
-		rank_ordre[barri]=int(np.sum(final_rank[barri]))
-
-	sort=(sorted(rank_ordre.items(), key=lambda x: x[1]))[::-1]
-	final=[]
-	i=1
-	for item in sort:
-		final.append((item[0],i))
-		i=i+1 if i<6 else 5
-
-	print(final)
-	###################################
-	# return {'recommendation': array_possible_neighbourhoods}
-	return final

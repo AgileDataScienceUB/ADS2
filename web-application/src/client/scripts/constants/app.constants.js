431bde473e2a2ef638a8716f44c4866de7f42bfe
DSolans <David.Solans@ca.com>
2017-12-15 12:38:14 +0100

    Intro.js not working as expected over the recommendation form. Slightly corrected

--- web-application/src/client/scripts/constants/app.constants.js
+++ web-application/src/client/scripts/constants/app.constants.js
@@ -1,4 +1,8 @@
 angular.module('ADS_Group2_Application')
 
+    // Use this in development
     .constant('API_BASE_URL', 'http://localhost:5000/api/');
+
+    // Use this in production
+    // .constant('API_BASE_URL', 'https://backend-whereshouldilive.herokuapp.com/api/');
 
